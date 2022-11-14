1bdaa9bfcd3e192e4d1cb307da97adb925df4978
CathalShandon <Cathalshandon@gmail.com>
2022-11-14 13:50:34 +0000

    added checkout forms

--- world_cup_jerseys/settings.py
+++ world_cup_jerseys/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'checkout'
 
     # Other
-    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -179,3 +179,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
