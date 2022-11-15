5d75a3e4ca0306c5951c2dfa9bef460726ddf320
CathalShandon <Cathalshandon@gmail.com>
2022-11-15 17:27:49 +0000

    added allowed host

--- world_cup_jerseys/settings.py
+++ world_cup_jerseys/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ble)m(g@9z5^1)%!&h#ub=05^1s61qlm!ozdbwx83rb#e=gu-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://world-cup-jerseys.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -118,12 +119,17 @@ WSGI_APPLICATION = 'world_cup_jerseys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
