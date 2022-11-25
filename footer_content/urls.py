b5974811b22156a6dbeefe8d734ed510c67a83ca
CathalShandon <Cathalshandon@gmail.com>
2022-11-25 19:30:09 +0000

    wire up views and models in footer

---
+++ footer_content/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('returns/', views.ReturnsContent, name='returns'),
+    path('postage/', views.PostageContent, name='postage'),
+    path('privacy/', views.PrivacyContent, name='privacy'),
+    path('about/', views.AboutContent, name='about'),
+]
+Footer
--- footer_content/views.py
+++ footer_content/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from .models import Returns, Postage, Privacy, About
 
-# Create your views here.
+
+def ReturnsContent(request):
+    """ A view that renders the returns and refunds page """
+
+    returns = Returns.objects.all()
+
+    context = {
+        'returns': returns,
+    }
+
+    return render(request, 'footer_content/returns.html', context)
+
+
+def PostageContent(request):
+    """ A view that renders the postage and packaging page """
+
+    postage = Postage.objects.all()
+
+    context = {
+        'postage': postage,
+    }
+
+    return render(request, 'footer_content/postage.html', context)
+
+
+def PrivacyContent(request):
+    """ A view that renders the privacy content page """
+
+    privacy = Privacy.objects.all()
+
+    context = {
+        'privacy': privacy,
+    }
+
+    return render(request, 'footer_content/privacy.html', context)
+
+
+def AboutContent(request):
+    """ A view that renders the about us content page """
+
+    about = About.objects.all()
+
+    context = {
+        'about': about,
+    }
+
+    return render(request, 'footer_content/about.html', context)
