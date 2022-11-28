2ce498ad6c44d486c8755288e70d2595a7edd37e
CathalShandon <Cathalshandon@gmail.com>
2022-11-28 21:36:38 +0000

    updated key

--- world_cup_jerseys/settings.py
+++ world_cup_jerseys/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ble)m(g@9z5^1)%!&h#ub=05^1s61qlm!ozdbwx83rb#e=gu-9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
     'django_quill',
 ]
 
