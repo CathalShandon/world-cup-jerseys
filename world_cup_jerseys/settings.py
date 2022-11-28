fc1f7b6f9a252cd989ce5c5e49c797063c45f6cc
CathalShandon <Cathalshandon@gmail.com>
2022-11-28 22:02:20 +0000

    change host

--- world_cup_jerseys/settings.py
+++ world_cup_jerseys/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://world-cup-jerseys.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['world-cup-jerseys.herokuapp.com', 'localhost']
 
 
 # Application definition
